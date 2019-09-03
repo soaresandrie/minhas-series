536cfef022ce9165bbd9ed81b633fada397d8d08
soaresandrie <jsoaresandrie@gmail.com>
2019-09-03 00:27:43 -0300

    criando um menu usando reactstrap

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React from 'react'
-
+import {
+  Navbar,
+  NavbarBrand,
+  Collapse,
+  Nav,
+  NavItem,
+  NavLink
+} from 'reactstrap'
 
 
 function App () {
   return (
-    <div className='App'>
-     <h1>Minhas Séries</h1>
+    <div>
+     <Navbar color='light' light expand='md'>
+      <NavbarBrand>Minhas Séries</NavbarBrand>
+      <Collapse isOpen={true} navbar>
+        <Nav className='ml-auto' navbar>
+          <NavItem>
+            <NavLink href='/'>Genêros</NavLink>
+          </NavItem>
+        </Nav>
+      </Collapse>
+     </Navbar>
     </div>
   )
 }
