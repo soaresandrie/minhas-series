522758ee7960833a7fdd980ec0523514813c109b
soaresandrie <jsoaresandrie@gmail.com>
2019-09-09 14:06:12 -0300

    adicionando o  minha-series server

--- src/App.js
+++ src/App.js
@@ -1,28 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Header from './Header'
+import Generos from './Generos'
+import axios from 'axios'
 import {
-  Navbar,
-  NavbarBrand,
-  Collapse,
-  Nav,
-  NavItem,
-  NavLink
-} from 'reactstrap'
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom'
+
+const Home = () => {
+  return <h1>Home</h1>
+}
+
 
 
 function App () {
+  const [data, setData] = useState({})
+  useEffect(() => {
+    axios.get('/api').then(res => {
+      setData(res.data)
+    })
+    //console.log(1)
+  }, [])
   return (
-    <div>
-     <Navbar color='light' light expand='md'>
-      <NavbarBrand>Minhas Séries</NavbarBrand>
-      <Collapse isOpen={true} navbar>
-        <Nav className='ml-auto' navbar>
-          <NavItem>
-            <NavLink href='/'>Genêros</NavLink>
-          </NavItem>
-        </Nav>
-      </Collapse>
-     </Navbar>
-    </div>
+    <Router>
+      <div>
+        <Header />
+        <Route path='/' exact component={Home} />
+        <Route path='/generos' component={Generos} />
+        <pre>{JSON.stringify(data)}</pre>
+      </div>
+    </Router>
   )
 }
 
---
+++ src/Generos.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+
+const Generos = () => {
+    return <h1>Generos</h1>
+}
+
+export default Generos
