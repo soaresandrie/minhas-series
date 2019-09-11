189556b838fc137a040c2f0f0b7f1eead509ad5d
soaresandrie <jsoaresandrie@gmail.com>
2019-09-11 19:13:35 -0300

    fim da primeira parte do app minhas series

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState, useEffect  } from 'react'
 import Header from './Header'
 import Generos from './Generos'
+import NovoGenero from './NovoGenero'
+import EditarGenero from './EditarGenero'
+
 import axios from 'axios'
 import {
   BrowserRouter as Router,
@@ -10,8 +13,6 @@ import {
 const Home = () => {
   return <h1>Home</h1>
 }
-
-
 
 function App () {
   const [data, setData] = useState({})
@@ -26,7 +27,9 @@ function App () {
       <div>
         <Header />
         <Route path='/' exact component={Home} />
-        <Route path='/generos' component={Generos} />
+        <Route path='/generos' exact component={Generos} />
+        <Route path='/generos/novo' exact component={NovoGenero} />
+        <Route path='/generos/:id' exact component={EditarGenero} />
         <pre>{JSON.stringify(data)}</pre>
       </div>
     </Router>
